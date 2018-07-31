ad795f77734d5f6d99412e7a48953ae6153b3f76
Lucie Daeye <lucied@mozillafoundation.org>
2018-07-31 14:20:53 +0200

    Add simple view + change URL to use path

--- superlists/urls.py
+++ superlists/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.urls import path
 from lists import views
 
-urlpatterns = [url(r"^$", views.home_page, name="home")]
+urlpatterns = [path("", views.home_page, name="home")]
