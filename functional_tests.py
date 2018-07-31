aaf1646f729f71e15e7e4984944419c9c150b7ea
Lucie Daeye <lucied@mozillafoundation.org>
2018-07-31 14:43:36 +0200

    update functional test

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,8 @@
+import time
 from selenium import webdriver
 import unittest
+
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -13,17 +16,28 @@ class NewVisitorTest(unittest.TestCase):
         # Can reach the homepage
         self.browser.get("http://localhost:8000")
 
-        # WebApp has correct name
+        # WebApp has correct name and header mentioning to-do lists
         self.assertIn("To-Do", self.browser.title)
-        self.fail("Finish the test!")
+        header_text = self.browser.find_element("h1").text
+        self.assertIn("To-Do", header_text)
 
         # Can enter item in to-do list straight away
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do iten")
 
         # Can enter content in a text box
+        inputbox.send_keys("Buy peacock feathers")
 
-        # Enter add item in the to-do list
+        # When user press enter, the page updates and list the added item in the to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertTrue(any(row.text == "1: Buy peacock feathers" for row in rows))
 
         # Can add more item in the to-do list
+        self.fail("Finish the test!")
 
         # If update the page, the items are still there
 
