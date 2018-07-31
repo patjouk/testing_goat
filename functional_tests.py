c110fd9f7db62b88b811baae2e92c951561b2286
Lucie Daeye <lucied@mozillafoundation.org>
2018-07-31 16:45:38 +0200

    Test refacto

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Can reach the homepage
         self.browser.get("http://localhost:8000")
@@ -32,9 +37,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
 
         # Can add more items in the to-do list
         inputbox = self.browser.find_element_by_id("id_new_item")
@@ -43,12 +46,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # When the page is updated, both items are listed
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
-        self.assertIn(
-            "2: Use peacock feathers to make a fly", [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
         # Each to-do has it's own URL, as explain to users
         self.fail("Finish the test!")
