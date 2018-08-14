531cbf21a5eb51db30dc05f2912366bf61bc659b
Lucie Daeye <lucied@mozillafoundation.org>
2018-08-14 15:37:47 +0200

    removed hardcorded urls in templates

--- lists/tests/test_view.py
+++ lists/tests/test_view.py
@@ -65,10 +65,7 @@ class ListViewTest(TestCase):
 
     def test_validation_errors_end_up_on_lists_page(self):
         list_ = List.objects.create()
-        response = self.client.post(
-            f"/lists/{list_.id}/",
-            data={"item_text": ""}
-        )
+        response = self.client.post(f"/lists/{list_.id}/", data={"item_text": ""})
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "list.html")
