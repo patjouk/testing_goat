41288abeac3a758ed5083250e6e24bacbf0c8375
Lucie Daeye <lucied@mozillafoundation.org>
2018-08-14 15:08:04 +0200

    Refacto list view: can add items

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -11,7 +11,12 @@ class ItemValidationTest(FunctionalTest):
         self.browser.find_element_by_id("id_new_item").send_keys(Keys.ENTER)
 
         # The home page refreshes: it has an error message saying empty items can't be added.
-        self.wait_for(lambda: self.assertEqual(self.browser.find_element_by_css_selector(".has-error").text, "You can't have an empty list item"))
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_element_by_css_selector(".has-error").text,
+                "You can't have an empty list item",
+            )
+        )
 
         # She decides to add an item with some text. It works.
         self.browser.find_element_by_id("id_new_item").send_keys("Buy milk")
@@ -23,7 +28,12 @@ class ItemValidationTest(FunctionalTest):
         self.browser.find_element_by_id("id_new_item").send_keys(Keys.ENTER)
 
         # It still doesn't work and she get the same error message.
-        self.wait_for(lambda: self.assertEqual(self.browser.find_element_by_css_selector(".has-error").text, "You can't have an empty list item"))
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_element_by_css_selector(".has-error").text,
+                "You can't have an empty list item",
+            )
+        )
 
         # She corrects her mistake and enter text.
         self.browser.find_element_by_id("id_new_item").send_keys("Make tea")
--- lists/tests/test_view.py
+++ lists/tests/test_view.py
@@ -9,6 +9,7 @@ class HomePageTest(TestCase):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
 
+
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
         list_ = List.objects.create()
@@ -38,6 +39,30 @@ class ListViewTest(TestCase):
 
         self.assertEqual(response.context["list"], correct_list)
 
+    def test_can_save_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            f"/lists/{correct_list.id}/",
+            data={"item_text": "A new item for an existing list"},
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new item for an existing list")
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirect_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            f"/lists/{correct_list.id}/",
+            data={"item_text": "A new item for an existing list"},
+        )
+        self.assertRedirects(response, f"/lists/{correct_list.id}/")
+
 
 class NewListTest(TestCase):
     def test_can_save_a_POST_request(self):
@@ -63,28 +88,3 @@ class NewListTest(TestCase):
         self.client.post("/lists/new", data={"item_text": ""})
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
-
-class NewItemTest(TestCase):
-    def test_can_save_POST_request_to_an_existing_list(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        self.client.post(
-            f"/lists/{correct_list.id}/add_item",
-            data={"item_text": "A new item for an existing list"},
-        )
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, "A new item for an existing list")
-        self.assertEqual(new_item.list, correct_list)
-
-    def test_redirect_to_list_view(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        response = self.client.post(
-            f"/lists/{correct_list.id}/add_item",
-            data={"item_text": "A new item for an existing list"},
-        )
-        self.assertRedirects(response, f"/lists/{correct_list.id}/")
