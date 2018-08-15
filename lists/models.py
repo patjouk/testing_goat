30d68a3eb7fc6cf236fd0718fdeb3ce0d668c3b2
Lucie Daeye <lucied@mozillafoundation.org>
2018-08-15 16:09:19 +0200

    add unique together for list and items

--- lists/models.py
+++ lists/models.py
@@ -8,5 +8,8 @@ class List(models.Model):
 
 
 class Item(models.Model):
+    class Meta:
+        unique_together = ("list", "text")
+
     text = models.TextField(default="")
     list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
