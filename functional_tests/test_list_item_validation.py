4eda1398bb84fdbbec1625c7d602aa6bf5030e9a
Lucie Daeye <lucied@mozillafoundation.org>
2018-08-15 12:28:17 +0200

    use form in all views. tests are green

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -7,37 +7,37 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         # Alice tries to add an empty item in the list and press Enter.
         self.browser.get(self.live_server_url)
-
         self.get_item_input_box().send_keys(Keys.ENTER)
 
-        # The home page refreshes: it has an error message saying empty items can't be added.
+        # The browser intercepts the request, and does not load the list page.
         self.wait_for(
-            lambda: self.assertEqual(
-                self.browser.find_element_by_css_selector(".has-error").text,
-                "You can't have an empty list item",
-            )
+            lambda: self.browser.find_element_by_css_selector("#id_text:invalid")
         )
 
-        # She decides to add an item with some text. It works.
+        # She start typing some text and the error disappears
         self.get_item_input_box().send_keys("Buy milk")
+        self.wait_for(
+            lambda: self.browser.find_element_by_css_selector("#id_text:valid")
+        )
 
+        # She can submit that new item successfully
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: Buy milk")
 
         # She tries to add another blank item.
         self.get_item_input_box().send_keys(Keys.ENTER)
 
-        # It still doesn't work and she get the same error message.
+        # Again, it doesn't work and the browser intercepts the request
+        self.wait_for_row_in_list_table("1: Buy milk")
         self.wait_for(
-            lambda: self.assertEqual(
-                self.browser.find_element_by_css_selector(".has-error").text,
-                "You can't have an empty list item",
-            )
+            lambda: self.browser.find_element_by_css_selector("#id_text:invalid")
         )
 
         # She corrects her mistake and enter text.
         self.get_item_input_box().send_keys("Make tea")
-
+        self.wait_for(
+            lambda: self.browser.find_element_by_css_selector("#id_text:valid")
+        )
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: Buy milk")
         self.wait_for_row_in_list_table("2: Make tea")
