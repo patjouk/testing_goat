678a5142d8f9594c01e5aa268537c70ab3ae4aba
Lucie Daeye <lucied@mozillafoundation.org>
2018-12-18 15:29:11 +0100

    Mock sending email in test

--- accounts/tests/test_view.py
+++ accounts/tests/test_view.py
@@ -1,10 +1,22 @@
 from django.test import TestCase
+from unittest.mock import patch
 
 
 class SendLoginEmailViewTest(TestCase):
-
     def test_redirects_to_home_page(self):
-        response = self.client.post("/accounts/send_login_email", data={
-            "email": "alex@example.com",
-        })
+        response = self.client.post(
+            "/accounts/send_login_email", data={"email": "alex@example.com"}
+        )
         self.assertRedirects(response, "/")
+
+    @patch("accounts.views.send_mail")
+    def tests_sends_mail_to_address_from_post(self, mock_send_email):
+        self.client.post(
+            "/accounts/send_login_email", data={"email": "alex@example.com"}
+        )
+
+        self.assertTrue(mock_send_email.called, True)
+        (subject, body, from_email, to_list), kwargs = mock_send_email.call_args
+        self.assertEqual(subject, "Your login link for Superlists")
+        self.assertEqual(from_email, "noreply@superlists")
+        self.assertEqual(to_list, ["alex@example.com"])
--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
-    path("send_login_email", views.send_login_email, name="send_login_email"),
-    ]
+    path("send_login_email", views.send_login_email, name="send_login_email")
+]
