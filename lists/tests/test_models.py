e22331bdebb16ddb6a584cef929566c76694f753
patjouk <lucie.daeye@gmail.com>
2019-01-25 14:31:03 +0100

    Lists have owners

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -45,3 +45,11 @@ class ListModelTest(TestCase):
     def test_get_absolute_url(self):
         list_ = List.objects.create()
         self.assertEqual(list_.get_absolute_url(), f"/lists/{list_.id}/")
+
+    def test_list_can_have_owner(self):
+        user = User.objects.create(email="a@b.com")
+        list_ = List.objects.create(owner=user)
+        self.assertIn(list_, user.list_set.all())
+
+    def test_list_owner_is_optional(self):
+        List.objects.create()  # should not rise
